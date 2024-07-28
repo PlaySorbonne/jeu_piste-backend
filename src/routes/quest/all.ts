153b50ad27d2198e0692189dddb5111b980c200f
Pablo <contact@pablo.town>
2024-07-29 01:19:09 +0200

    add /quest/all route

--- src/routes/quest/all.ts
+++ src/routes/quest/all.ts
@@ -1,1 +1,24 @@
-// get all accessible quests for a player
+import express from "express";
+import { RequestWPrisma, ResponseTyped } from "../../utils/types";
+import { HttpCodes } from "../../utils/constants";
+import { requireAuth } from "../../services/middlewares";
+
+const router = express.Router();
+
+router.get(
+  "/all",
+  requireAuth({ admin: true }),
+  async (req: RequestWPrisma, res: ResponseTyped) => {
+    let all = await req.prisma!.quest.findMany();
+
+    let code = HttpCodes.OK;
+    res.status(code).json({
+      message: "all quests",
+      status: code,
+      isError: false,
+      data: all,
+    });
+  }
+);
+
+export default router;
--- src/routes/quest/index.ts
+++ src/routes/quest/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
 import Create from "./create";
+import All from "./all";
 
 const router = express.Router();
 router.use(Create);
+router.use(All);
 
 router.get("/", (req, res) => {
   res.status(200).send("user");
