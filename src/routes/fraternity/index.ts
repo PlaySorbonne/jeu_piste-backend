999a0f3b8cacdc0bfa4fd3ec2a7b2520ec6ee6d4
Pablo <contact@pablo.town>
2024-08-02 23:51:04 +0200

    add modify route for fraternities (nor working for the moment :( )

--- src/routes/fraternity/index.ts
+++ src/routes/fraternity/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 
 import All from "./all";
+import Modify from "./modify";
 
 const router = express.Router();
 
 router.use(All);
+router.use(Modify);
 
 router.get("/", (req, res) => {
   res.status(200).send("fraternité");
